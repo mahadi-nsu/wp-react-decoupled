9a3966c09a429e6d42b2e68bc622f2a6f5894a20
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-12-26 23:21:12 +0600

    custom hooks added for api call

--- src/components/AllPost/AllPost.js
+++ src/components/AllPost/AllPost.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Center } from "@chakra-ui/react";
 import Post from "../Post/Post";
 import styles from "./AllPost.module.css";
+import useFetch from "../../services/useApiRequest";
 
 const AllPost = () => {
+  const blogUrl =
+    "https://dev-first-wp-test.pantheonsite.io/wp-json/wp/v2/posts/";
+    
+  const { data, loading, error } = useFetch(blogUrl);
+
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
   return (
     <Center className={styles.mainContainer}>
       <div className={styles.heroContainer}>
