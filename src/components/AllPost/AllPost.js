d923a311d0b9b7037b235b20ec5e55d17faecdd6
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-01-04 20:07:34 +0600

    toggleable modal added

--- src/components/AllPost/AllPost.js
+++ src/components/AllPost/AllPost.js
@@ -3,13 +3,20 @@ import { Grid, Center } from "@chakra-ui/react";
 import Post from "../Post/Post";
 import styles from "./AllPost.module.css";
 import useFetch from "../../services/useApiRequest";
-import { PhoneIcon, AddIcon, WarningIcon } from "@chakra-ui/icons";
+import { MinusIcon, AddIcon, WarningIcon } from "@chakra-ui/icons";
+import Modal from "../common/modal/Modal";
 
 const AllPost = () => {
   const blogUrl =
     "https://dev-first-wp-test.pantheonsite.io/wp-json/wp/v2/posts";
 
   const { data, loading, error } = useFetch(blogUrl);
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const postBlogHandler = () => {
+    setIsClicked(!isClicked);
+  };
 
   if (loading && data == null) return <div>Loading...</div>;
 
@@ -20,8 +27,26 @@ const AllPost = () => {
       </div>
       <div className={styles.heroContainer}>
         <div className={styles.addIconContainer}>
-          <AddIcon w={12} h={12} color="green.700" className={styles.addIcon} />
+          {!isClicked ? (
+            <AddIcon
+              w={12}
+              h={12}
+              color="green.700"
+              className={styles.addIcon}
+              onClick={postBlogHandler}
+            />
+          ) : (
+            <MinusIcon
+              w={12}
+              h={12}
+              color="red.700"
+              className={styles.addIcon}
+              onClick={postBlogHandler}
+            />
+          )}
         </div>
+
+        <div className={styles.modal__appear}>{isClicked ? <Modal /> : ""}</div>
         <Grid
           templateColumns="repeat(3, 1fr)"
           gap={1}
---
+++ src/components/common/modal/Modal.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Modal.module.css";
+
+const Modal = () => {
+  return <div className={styles.modal_hero}>this is modal</div>;
+};
+
+export default Modal;
